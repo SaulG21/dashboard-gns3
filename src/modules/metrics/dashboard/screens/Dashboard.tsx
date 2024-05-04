d208e11abcb4d3c14b304a8b4e8355b4f8a8c801
SaulG21 <victor.garcia@cua.uam.mx>
2024-05-03 19:49:11 -0600

    S.G tests and modification in Graph2.tsx

--- src/modules/metrics/dashboard/screens/Dashboard.tsx
+++ src/modules/metrics/dashboard/screens/Dashboard.tsx
@@ -2,11 +2,13 @@ import Button from "@mui/material/Button";
 import { hostNameRouter, uniqueMAC, DataArpPops } from "../../functions/FindBoundaries"
 import { useEffect, useState } from "react";
 import CircularProgress from "@mui/material/CircularProgress";
+import Graph from "../../../graphs/components/Graph2";
 
 export default function Dashboard() {
     const hostName = hostNameRouter;
     // const table = arpTable["Cisco-IOS-XE-arp-oper:arp-data"]["arp-vrf"][0]["arp-oper"];
     const [neighbor, setNeighbor] = useState<DataArpPops[]>([]);
+    const [view, setView] = useState(false);
 
     // const updateNeighbor = function () {
     //     setNeighbor(uniqueMAC);
@@ -19,7 +21,7 @@ export default function Dashboard() {
     //    .then(err=>console.log(err))
     // },[]);
 
-    const makeRequest = async ()=>{
+    const makeRequest = async () => {
         const res = await fetch('http://localhost:4000/',
             {
                 method: "GET",
@@ -31,37 +33,25 @@ export default function Dashboard() {
 
     return (
         <div className="flex h-full w-full justify-center items-center bg-gray-100">
-            <div className="flex flex-col w-full items-center justify-center space-y-3">
-                <p>
-                    Topología de Red
-                </p>
-                <Button
-                    onClick={makeRequest}
-                    variant="contained"
-                >
-                    Ver informacion
-                </Button>
+            <div className="flex flex-col w-full items-center justify-center space-y-3 border ">
                 {
-                    neighbor && (
+                    view ? (
                         <div className="flex flex-col w-full border">
-                            <p>Vecinos de router {hostName}: </p>
-                            {
-                                neighbor ? (
-                                    neighbor.map((item) => {
-                                        return (
-                                            <div className="flex w-full flex-col my-2"
-                                                key={item.hardware}
-                                            >
-                                                <p>Direccion IP: {item.address}</p>
-                                                <p>Direccion MAC: {item.hardware}</p>
-                                                <p>Nombre Interfaz: {item.interface}</p>
-                                                <p>Tipo: {item.type}</p>
-                                            </div>
-                                        );
-                                    })
-                                ) : null
-                            }
+                            <Graph />
                         </div>
+                    ) : (
+                        <>
+                            <p>Vecinos de router {hostName}: </p>
+                            <p>
+                                Topología de Red
+                            </p>
+                            <Button
+                                onClick={()=>{setView(true)}}
+                                variant="contained"
+                            >
+                                Ver informacion
+                            </Button>
+                        </>
                     )
                 }
             </div>
